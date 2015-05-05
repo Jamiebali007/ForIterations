7de7d1b996edf61ccab2cc82bba8ab26692e3b86
Jamiebali007 <PPGJamie@gmail.com>
2015-05-05 18:04:06 +0100

    Bob

--- ForIterations/ForIterations/Module1.vb
+++ ForIterations/ForIterations/Module1.vb
@@ -1,9 +1,9 @@
 ﻿Module Module1
     Dim Index As Integer
     Sub Main()
-        For Index = 1 To 10
-            If Index = 7 Then
-                Console.WriteLine("Found Seven")
+        For Index = 1 To 20
+            If Index = 17 Then
+                Console.WriteLine("Found Seventeen")
             End If
             Console.WriteLine(Index)
         Next
