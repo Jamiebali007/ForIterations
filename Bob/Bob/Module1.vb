c8691f5f4a5ba184d383d3ccc6eece483e5a71e8
Jamiebali007 <PPGJamie@gmail.com>
2015-05-05 17:59:17 +0100

    Bob

---
+++ Bob/Bob/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+    Dim Bob As String
+    Sub Main()
+        Bob = Console.ReadLine
+        If Bob = Bob Then
+            Console.WriteLine("You Win! BOB!")
+            Console.ReadKey()
+        Else
+            Console.WriteLine("YOU LOSE! YOU GET NOTHING! GOOD DAY SIR!")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
