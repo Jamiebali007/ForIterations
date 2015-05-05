b7447a4298a4f56e23e8ed545556156af08ab59d
SuddenReality <stotfold@hotmail.co.uk>
2015-05-05 17:35:29 +0100

    Added a Iteration Loop to code, to print out when achieve seven to console!

--- ForIterations/ForIterations/Module1.vb
+++ ForIterations/ForIterations/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
-
+    Dim Index As Integer
     Sub Main()
-
+        For Index = 1 To 10
+            If Index = 7 Then
+                Console.WriteLine("Found Seven")
+            End If
+            Console.WriteLine(Index)
+        Next
+        Console.ReadKey()
     End Sub
 
 End Module
